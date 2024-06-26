8d4053805b7dfc7c24b1bff9e17c5619b2fc07ba
Yeshwanthi16 <yashuin@gmail.com>
2024-06-26 07:42:36 +0530

    files added

---
+++ src/main/java/com/example/bidding/Model/AbstractEntity.java
@@ -0,0 +1,20 @@
+package com.example.bidding.Model;
+
+import jakarta.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.UUID;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
+import lombok.EqualsAndHashCode;
+
+@MappedSuperclass
+@EqualsAndHashCode(of = "uuid")
+public class AbstractEntity implements Serializable {
+
+    @NotNull
+    private String uuid = UUID.randomUUID().toString();
+
+    @Version
+    private Long version;
+
+}
---
+++ src/main/java/com/example/bidding/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.bidding.repository;
+
+import java.util.Optional;
+
+import com.example.bidding.Model.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
